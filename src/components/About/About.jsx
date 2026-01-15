83ce31a183bb6b6ed266773c4e32abf74910e788
Maria Veres <veres.maria.dev@gmail.com>
2026-01-15 12:42:59 +0300

    feat: upd about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,10 +2,18 @@ import './About.css';
 
 export const AboutMe = () => (
   <section id="about" className="about-section">
-      <p>Hello! I am a developer with 3 years of&nbsp;experience working in&nbsp;a <a href="https://intuition.team/about" target='_blanc'>design bureau</a> team.<br/> 
-      It is important for me to create interfaces that not&nbsp;only look good, but are also accessible, well-structured and&nbsp;easy to&nbsp;maintain. 
+      <p>Hello! I am a developer with 3,5 years of&nbsp;experience working with React and JavaScript, building component-based interfaces, implementing client-side routing, and creating interactive UI.
+        </p><br/><p>
+        I care about accessibility, performance and Core Web Vitals, and enjoy collaborating with a&nbsp;team to turn ideas into clean, user-focused interfaces.<br/>
+      </p><br/><p>
+      If you value both quality and teamwork, I’d be happy to connect.
+     
+{/* 
+      My background includes strong experience with semantic markup and styling, combined with JavaScript-driven interaction, performance optimization, and component-based development. <br/>
+      I pay close attention to performance and usability, including optimizing Core Web Vitals, and collaborate closely with the team to ensure the final product aligns with both user needs and product goals.<br/>
+      It is important for me to create interfaces that not&nbsp;only look good, but are also accessible, well-structured and&nbsp;easy to&nbsp;maintain.  */}
       </p>
     
-      <p>Based in Amsterdam, NL.</p>
+      <p className="location">Based in Amsterdam, NL.</p>
   </section>
 );
