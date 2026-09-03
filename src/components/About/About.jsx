5f34dee35b300e949968932d467a85738a718b4a
Maria Veres <veres.maria.dev@gmail.com>
2026-09-03 12:14:12 +0200

    feat: add email

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,18 +2,15 @@ import './About.css';
 
 export const AboutMe = () => (
   <section id="about" className="about-section">
-      <p>Hello! I’m a developer with 3+ years of&nbsp;experience building responsive and&nbsp;interactive web interfaces with SvelteKit,&nbsp;React, and TypeScript.
+      <p>Hello! I’m a developer with 3+ years of&nbsp;experience building responsive and&nbsp;interactive web interfaces with React, SvelteKit and TypeScript.
         </p><p>
         I enjoy creating maintainable component-based solutions, improving performance, and&nbsp;transforming designs into accessible, user-friendly products.
       </p><p>
       Feel free to get in touch, I’d love to discuss interesting projects and opportunities!
-     
-{/* 
-      My background includes strong experience with semantic markup and styling, combined with JavaScript-driven interaction, performance optimization, and component-based development. <br/>
-      I pay close attention to performance and usability, including optimizing Core Web Vitals, and collaborate closely with the team to ensure the final product aligns with both user needs and product goals.<br/>
-      It is important for me to create interfaces that not&nbsp;only look good, but are also accessible, well-structured and&nbsp;easy to&nbsp;maintain.  */}
+      
       </p>
-    
+      <p className="location">veres.maria.dev@gmail.com</p>
       <p className="location">Based in Amsterdam, NL.</p>
+
   </section>
 );
